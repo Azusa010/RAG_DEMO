b248ec05772db8f197814da3fb660c1140f27444
Azusa <m3171470760@gmail.com>
2026-08-04 11:58:04 +0800

    2

--- processor/import_processor/nodes/node_enrty.py
+++ processor/import_processor/nodes/node_enrty.py
@@ -1,10 +1,11 @@
+# processor/import_processor/nodes/node_entry.py
+import json
 import logging
 from pathlib import Path
 
 from processor.import_processor.base import BaseNode, setup_logging
-from processor.import_processor.state import ImportGraphState
 from processor.import_processor.exceptions import StateFieldError, FileProcessingError, ValidationError
-import json
+from processor.import_processor.state import ImportGraphState
 
 
 class NodeEntry(BaseNode):
@@ -15,21 +16,22 @@ class NodeEntry(BaseNode):
     name = "node_entry"
 
     def process(self, state: ImportGraphState):
+        # 1.从state中获取文件的绝对路径
         import_file_path = state.get("import_file_path")
+        # 判断路径是否为空
         if not import_file_path:
             raise StateFieldError(
-                node_name='node_entry',
                 field_name="import_file_path",
-                message="导入文件路径不能为空",
+                message="文件路径不能为空",
                 expected_type=str)
 
+        #2.将import_file_path转换为Path对象
         import_file_path_obj = Path(import_file_path)
 
         if not import_file_path_obj.exists():
-            raise FileProcessingError(
-                message=f"文件{import_file_path_obj.name}不存在"
-            )
+            raise FileProcessingError(message=f"文件{import_file_path_obj.name}不存在")
 
+        # 3.判断文件类型
         if import_file_path_obj.suffix == ".pdf":
             state["is_pdf_read_enabled"] = True
             state["pdf_path"] = import_file_path
@@ -37,21 +39,28 @@ class NodeEntry(BaseNode):
             state["is_md_read_enabled"] = True
             state["md_path"] = import_file_path
         else:
-            raise ValidationError(
-                message=f"不支持的文件类型{import_file_path_obj.suffix}"
-            )
+            raise ValidationError(message=f"不支持的文件类型{import_file_path_obj.suffix}")
 
+        #4. 获取文件名作为标题
         state["file_title"] = import_file_path_obj.stem
 
         return state
 
 
 if __name__ == "__main__":
-    node_entry = NodeEntry()
+    # 激活日志的全局配置
     setup_logging()
-    state = {
-        "import_file_path": r"D:\qdd\hello_RAG\processor\doc\Aolynk CB304n Cable网桥 用户手册-5W100-整本手册.pdf"
+
+    # 初始化图状态
+    init_state = {
+        "import_file_path": r"D:\doc\hak180产品安全手册.pdf"
     }
-    result = node_entry(state)
+
+    # 创建节点对象
+    node_entry = NodeEntry()
+    # 执行节点的单元测试
+    result = node_entry(init_state)
+    # 将返回的图状态进行json序列化
     json_state = json.dumps(result, ensure_ascii=False, indent=4)
+    # 输出
     logging.getLogger().info(json_state)
