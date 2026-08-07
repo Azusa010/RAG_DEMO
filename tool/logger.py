592cf8f57f73a63b74c5d5a96a54c73281beea78
Azusa <m3171470760@gmail.com>
2026-08-07 10:31:56 +0800

    feat:骨架节点

--- tool/logger.py
+++ tool/logger.py
@@ -8,7 +8,7 @@ logger.setLevel(logging.INFO)
 #定义日志记录器的颜色和格式
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
-    '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    '%(log_color)s%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     log_colors={
         'DEBUG': 'cyan',
